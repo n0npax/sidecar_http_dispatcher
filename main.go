5314c2929d2add2f6bfc9df57b25a8a4add56e6a
Marcin Niemira <marcin.niemira@gmail.com>
2020-05-23 12:38:44 +1000

    hardcoded v1 go

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
@@ -13,16 +15,44 @@ func main() {
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+
+	// TODO setup nice params
 	client := &http.Client{}
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+
 		req, err := http.NewRequest("GET", "http://example.com", nil)
 		if err != nil {
-			panic("cannot create a req")
+			panic(err)
 		}
+		req.Header.Add("sidecar_http_dispatched", "true")
+		for k, v := range r.Header {
+			fmt.Printf("-> %v %v\n", k, v[0])
+			req.Header.Add(k, v[0])
+		}
+
 		resp, _ := client.Do(req)
-		w.Write([]byte(fmt.Sprintf("%v", resp)))
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		w.Write(body)
+
+		for k, v := range resp.Header {
+			fmt.Printf("<- %v %v\n", k, v[0])
+			w.Header().Add(k, v[0])
+		}
 	})
 
-	http.ListenAndServe(":3333", r)
+	http.ListenAndServe(getenv("SIDECAR_PORT", ":5000"), r)
+}
+
+func getenv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
 }
